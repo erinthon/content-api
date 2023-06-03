f45ec0e59d75bd844da12c8e8b73dc6388506d17
Erinthon Colnaghi <erinthonc@gmail.com>
2023-06-03 10:25:55 -0300

    - teste

---
+++ src/main/java/com/erinthon/coreai/CoreaiApplication.java
@@ -0,0 +1,13 @@
+package com.erinthon.coreai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erinthon/coreai/CoreaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erinthon.coreai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
